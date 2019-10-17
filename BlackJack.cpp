d6c7d87b8744a4a823dda91dcc38ec54e4c2e84b
IllugiSvensson <woljin1@mail.ru>
2019-10-17 06:29:39 +0300

    .

---
+++ BlackJack.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Card {
+private:
+
+public:
+
+};
+
+class Hand {
+private:
+
+public:
+
+};
+
+class Deck: public Hand {
+private:
+
+public:
+
+};
+
+class GenericPlayer: public Hand {
+private:
+
+public:
+
+};
+
+class Player: public GenericPlayer {
+private:
+
+public:
+
+};
+
+class House: public GenericPlayer {
+private:
+
+public:
+
+};
+
+class Game {
+private:
+
+public:
+
+};
+
+
+
+int main(int argc, char *argv[])  {
+
+
+
+return 0;
+}
